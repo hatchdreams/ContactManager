fa5b657b0d42cf9c631ca0f0247ea8e91b634efa
hatchdreams <67721215+hatchdreams@users.noreply.github.com>
2024-02-07 18:36:11 -0500

    Get Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ContactManager';
+  title = 'Contact Manager';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://my.api.mockaroo.com/customers.json?key=03c46990').subscribe({
+      next: response => console.log(response),
+      error: error=> console.log(error),
+      complete: () => {
+        console.log('request completed');
+        console.log('extra statement');
+      }
+    })
+  }
 }
