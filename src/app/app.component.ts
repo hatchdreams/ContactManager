61206f74fd5ed59f2d50a32f4f8358820912cfdd
hatchdreams <67721215+hatchdreams@users.noreply.github.com>
2024-02-07 21:20:04 -0500

    Interfaces & HttpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Customer } from './models/customer';
 
 @Component({
   selector: 'app-root',
@@ -7,17 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Contact Manager';
+  title = 'Customer Manager';
+  customers: Customer[] = []
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get('https://my.api.mockaroo.com/customers.json?key=03c46990').subscribe({
-      next: response => console.log(response),
+    this.http.get<Customer[]>('https://my.api.mockaroo.com/customers.json?key=03c46990').subscribe({
+      next: response => {
+        this.customers = response;
+      },
       error: error=> console.log(error),
       complete: () => {
         console.log('request completed');
-        console.log('extra statement');
       }
     })
   }
