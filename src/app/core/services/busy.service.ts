aa55f4afffd30fee974c8554d162575108d4f1ec
hatchdreams <67721215+hatchdreams@users.noreply.github.com>
2024-02-14 01:41:29 -0500

    Update busy.service.ts

--- src/app/core/services/busy.service.ts
+++ src/app/core/services/busy.service.ts
@@ -14,7 +14,7 @@ export class BusyService {
     this.spinnerService.show(undefined, {
       type: 'pacman',
       bdColor: 'rgba(255,255,255,0.7)',
-      color: '#333333'
+      color: '#FFF'
     })
   }
 
